84907a8670b7e95a36f554087269f1435d2fd55a
Jack Branch <jackbranch@Jacks-MacBook-Pro.local>
2018-08-15 18:12:29 +0100

    updating binary search code

--- src/lib/KarateKata.java
+++ src/lib/KarateKata.java
@@ -8,9 +8,35 @@ public class KarateKata {
 	// next only consider the chosen half, next halve again
 	// stop when no more array or value is found
 	
-	public static int chop(int inputInteger, int[] intArray) {
+	public static int[] chop(int inputInteger, int[] intArray) {
 		
-		return 0;
+		int totalLength = intArray.length;
+		
+		int mid = totalLength / 2;
+		int[] result = {-1, 0};
+		
+		// add loop for array search
+		
+		// 1st half
+		for(int i = 0; i < mid; i ++) {
+			if(intArray[i] == inputInteger) {
+				result[0] = 1;
+				result[1] = i;
+				return result;
+			}
+		}
+		
+		//2nd half
+		for(int i = totalLength; i > mid; i --) {
+			if(intArray[i] == inputInteger) {
+				result[0] = 1;
+				result[1] = i;
+				return result;
+			}
+		}
+		
+		
+		return result;
 
 	}
 }
