ad101604b58c38ab5499333fe8b72db62d23eb19
Jack Branch <jackbranch@Jacks-MacBook-Pro.local>
2018-08-15 17:51:41 +0100

    1st commit

---
+++ src/lib/KarateKata.java
@@ -0,0 +1,16 @@
+package lib;
+
+public class KarateKata {
+	
+	// binary chop
+	// find the position of value  in sorted array of values
+	// Multiple passes - first if value is in top or bottom half in list
+	// next only consider the chosen half, next halve again
+	// stop when no more array or value is found
+	
+	public static int chop(int inputInteger, int[] intArray) {
+		
+		return 0;
+
+	}
+}
---
+++ src/lib/KarateKata_test.java
@@ -0,0 +1,14 @@
+package lib;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class KarateKata_test {
+
+	@Test
+	void test() {
+		
+	}
+
+}
