c117d2c3deaacb13c8e292b2714df8511d9c3ba0
Jack Branch <jackbranch@Jacks-MacBook-Pro.local>
2018-08-16 14:59:03 +0100

    4 methods, some bugs but done for now

--- src/lib/KarateKata.java
+++ src/lib/KarateKata.java
@@ -1,5 +1,7 @@
 package lib;
 
+import java.util.ArrayList;
+
 public class KarateKata {
 	
 	// binary chop
@@ -7,36 +9,73 @@ public class KarateKata {
 	// Multiple passes - first if value is in top or bottom half in list
 	// next only consider the chosen half, next halve again
 	// stop when no more array or value is found
+	// see KarateKata_test for results
 	
-	public static int[] chop(int inputInteger, int[] intArray) {
+	// runs from front to end
+	public static int chop1(int inputInteger, ArrayList<Integer> intArray) {
 		
-		int totalLength = intArray.length;
-		
-		int mid = totalLength / 2;
-		int[] result = {-1, 0};
-		
-		// add loop for array search
-		
-		// 1st half
-		for(int i = 0; i < mid; i ++) {
-			if(intArray[i] == inputInteger) {
-				result[0] = 1;
-				result[1] = i;
+		int totalLength = intArray.size();
+		int result = -1;
+		for(int i = 0; i <totalLength; i ++) {
+			if(intArray.get(i) == inputInteger) {
+				result = i;
 				return result;
 			}
 		}
-		
-		//2nd half
-		for(int i = totalLength; i > mid; i --) {
-			if(intArray[i] == inputInteger) {
-				result[0] = 1;
-				result[1] = i;
-				return result;
-			}
-		}
-		
-		
 		return result;
-
+	}
+	
+	// runs from mid to front, mid to back
+	public static int chop2(int inputInteger, ArrayList<Integer> intArray) {
+		int result = -1;
+		int totalLength = intArray.size();
+		int mid = totalLength/2;
+		//first half
+		for(int i = mid; i > 0; i --) {
+			if(intArray.get(i) == inputInteger) {
+				return i;
+			}
+		}
+		for(int i = mid; i < totalLength; i ++) {
+			if(intArray.get(i) == inputInteger) {
+				return i;
+			}
+		}
+		return result;
+	}
+	
+	// Chooses which end to search based on size of inputInt and uses above method
+	public static int chop3(int inputInteger, ArrayList<Integer> intArray) {
+		int end = intArray.size();
+		int mid = end / 2;
+		int start = 0;
+		if(inputInteger > mid) {
+			start = mid ;
+		}
+		for(int i = start; i < end - (50 - start); i ++) {
+			if(intArray.get(i) == inputInteger) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	// calculates how much bigger last value is than input, uses this to set start position
+	public static int chop4(int inputInteger, ArrayList<Integer> intArray) {
+		int length = intArray.size();
+		float ratio = (intArray.get(length - 1) *100/ inputInteger) ; 
+		int startPos = Math.round(length / (ratio / 100));
+		System.out.println(startPos);
+		for(int i = startPos; i > 0; i --) {
+			if(intArray.get(i) == inputInteger) {
+				return i;
+			}
+		}
+		// in case 1st missed
+		for(int i = 0; i < startPos; i ++) {
+			if(intArray.get(i) == inputInteger) {
+				return i;
+			}
+		}
+		return -1;
 	}
 }
--- src/lib/KarateKata_test.java
+++ src/lib/KarateKata_test.java
@@ -2,13 +2,59 @@ package lib;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class KarateKata_test {
+	
+	public KarateKata karateKata;
+	public ArrayList<Integer> sortArray = new ArrayList<Integer>();
+	public int expected;
+	public int inputInt;
+	@BeforeEach
+	void initialize() {
+		Random rand = new Random();
+		for(int i = 0; i < 10000000; i++) {
+			int n = rand.nextInt(100000000 + 1);
+			sortArray.add(n);
+		}
+		Collections.sort(sortArray);
+		int randindex = rand.nextInt(10000000 - 1);
+		inputInt = sortArray.get(randindex);
+		expected = randindex;
+	}
+	
+	void searchMethodTest(int resultInput) {
+		assertEquals(resultInput, expected);
+	}
 
 	@Test
-	void test() {
-		
+	void chop1Test() {
+		searchMethodTest(KarateKata.chop1(inputInt, sortArray));
+		// time for test 8 - 10s
+	}
+
+	@Test
+	void chop2Test() {
+		searchMethodTest(KarateKata.chop2(inputInt, sortArray)); // avrg fastest excluding 4
+		// time for test 5.2 - 8s 
+	}
+	
+	@Test
+	void chop3Test() {
+		searchMethodTest(KarateKata.chop3(inputInt, sortArray));
+		// time fore test 5.0s - 7.5s
+	}
+	
+	@Test
+	void chop4Test() {
+		searchMethodTest(KarateKata.chop4(inputInt, sortArray)); // can be buggy
+		// time fore test 4.8 - 5.6s
 	}
 
 }
